d65be6e690e49b6e6b036194698d5a07e0b9af51
Xinyi Ye <xinyi.ye@192.168.0.2>
2020-08-01 17:47:26 +0800

    [xinyi] .sql to sql/

---
+++ sql/select.sql
@@ -0,0 +1,2 @@
+SELECT * FROM [SybottDB].[dbo].[ZIPCodes]
+WHERE ZipCode='501' AND City='HOLTSVILLE' AND County='SUFFOLK' AND CityAliasName='HOLTSVILLE' AND PreferredLastLineKey='V13916' AND CityStateKey='V13916';
