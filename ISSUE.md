4474168ce4c0b8bd84f818bfb3e54b4bcfa224c3
Xinyi Ye <xinyi.ye@192.168.0.2>
2020-08-01 18:23:43 +0800

    [xinyi] [add] NLog

--- ISSUE.md
+++ ISSUE.md
@@ -1,5 +1,4 @@
-- **How to delete a row from DataTable and update to SQL Server?**
-- **Why the delete result does not update to SQL Server?**
+## Delete a row from DataTable and update to SQL Server
 
 See more details from [this](http://www.mamicode.com/info-detail-2523323.html)
 
@@ -7,7 +6,7 @@ Do not use *DataTable.Rows.Remove(row)*, which equals to *DataTable.Rows[rowInde
 
 
 
-- **read from .xls or .tab?**
+## Read from .xls or .tab?
 
 At first, I tried to extract data from .xls to DataTable
 
@@ -39,4 +38,41 @@ when I tried to open this .xls file, I got the prompt window. This is probably t
 <img src="https://github.com/Mercy811/ZipCodeDatabaseUpdate/blob/master/img/xlsformatnotmatch.png" height="50%" width="50%"/>
 
 So I read from .tab at last.
+
+
+
+## NLog with VSCode
+
+Here are two ways to use NLog in .net core application: 
+
+1. use nlog.config
+2. [configure in the code](https://github.com/NLog/NLog/wiki/Configure-from-code)
+
+When I tried to follow the [documentation](https://github.com/NLog/NLog/wiki/Configure-from-code), it didn't work. The problem is that the configuration file was not included to complie. And the key is the sentence in ***Description/2. Create a nlog.config file***: 
+
+> (File Properties: Copy Always)
+
+However, instead of using VSCode, the tutorial uses Visual Studio which is able to set this property easily, just clicking the file and reseting the property. 
+
+In VScode, file property could set in .csproj file. For example, in <ItemGroup> add <Content> element and <CopyToOutputDirectoy> element. According to [VS documentation](https://docs.microsoft.com/en-us/visualstudio/msbuild/propertygroup-element-msbuild?view=vs-2019) making such modificaiton in .csproj is equvilant to directly clicking the file and setting. 
+
+```
+<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <OutputType>Exe</OutputType>
+    <TargetFramework>netcoreapp3.1</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Include="NLog" Version="4.7.3" />
+    <Content Include="NLog.config">
+      <CopyToOutputDirectory>Always</CopyToOutputDirectory>
+    </Content>
+  </ItemGroup>
+
+</Project>
+```
+
+
 
--- update.cs
+++ update.cs
@@ -5,11 +5,9 @@ using System.IO.Compression;
 using System.Data.SqlClient;
 using System.Data;
 using System.Globalization;
-using System.Collections;
+using NLog;
 
 using CsvHelper;
-
-using Updating;
 
 public static class DBUpdate
 {
@@ -17,6 +15,10 @@ public static class DBUpdate
     private static string _zipFileName = "2020-06-Update-STANDARD.zip";
     private static string _unzipFileDirectory = _directory+ "2020-06-Update-STANDARD/";
     private static string _tabFileName = "2020-06-Update-STANDARD.tab";
+
+
+    private static Logger logger = LogManager.GetCurrentClassLogger();
+
 
     public static string DownloadFile(string url)
     {
@@ -40,7 +42,6 @@ public static class DBUpdate
     {
         ZipFile.ExtractToDirectory(_directory+_zipFileName,_unzipFileDirectory);
     }
-
 
     public static void AddDataRow(DataTable myTable, UpdateTable myUpdateTable)
     {
@@ -77,13 +78,13 @@ public static class DBUpdate
         myRow["FinanceNumber"]= myUpdateTable.FinanceNumber;
         myRow["CountyMixedCase"]= myUpdateTable.CountyMixedCase;
         myTable.Rows.Add(myRow);
-        Console.WriteLine("Add: ZipCode='"+myRow["ZipCode"]+"' AND City='"+myRow["City"]+"' AND County='"+myRow["County"]+"' AND CityAliasName='"+myRow["CityAliasName"]+"' AND PreferredLastLineKey='"+myRow["PreferredLastLineKey"]+"' AND CityStateKey='"+myRow["CityStateKey"]+"'");
+        logger.Info("Add: ZipCode='"+myRow["ZipCode"]+"' AND City='"+myRow["City"]+"' AND County='"+myRow["County"]+"' AND CityAliasName='"+myRow["CityAliasName"]+"' AND PreferredLastLineKey='"+myRow["PreferredLastLineKey"]+"' AND CityStateKey='"+myRow["CityStateKey"]+"'");
     }
 
     public static void DeleteDataRow(DataTable myTable, UpdateTable myUpdateTable)
     {
         string selectCondition = "ZipCode='"+Int32.Parse(myUpdateTable.ZipCode).ToString()+"' AND City='"+myUpdateTable.City+"' AND County='"+myUpdateTable.County+"' AND CityAliasName='"+myUpdateTable.CityAliasName+"' AND PreferredLastLineKey='"+myUpdateTable.PreferredLastLineKey+"' AND CityStateKey='"+myUpdateTable.CityStateKey+"'";
-        Console.WriteLine("DELETE: "+selectCondition);
+        logger.Info("DELETE: "+selectCondition);
         DataRow[] myRows = myTable.Select(selectCondition);
         foreach (var row in myRows)
             row.Delete();
@@ -125,80 +126,103 @@ public static class DBUpdate
         public string  CountyMixedCase { get; set; }
 
     }
-    
+
     public static void Main(string[] args)
     {
-        // download 
-        Console.WriteLine("==================1. DOWNLOAD==================");
-        string url = "https://files.zip-codes.com/_updates_a9dh38dyq6ek/2020-06-01_UPDATE_UZJFV7B6/2020-06-Update-STANDARD.zip";
-        string fileName = DBUpdate.DownloadFile(url);
-        if (!String.IsNullOrEmpty(fileName))
+        try
         {
-            Console.WriteLine("Successfully download '" + fileName+"'");
-        }
-        else
-        {
-            Console.WriteLine("Failed to download '"+fileName+"'");
-            // Application.Exit();
-        }    
-        Console.WriteLine("press Enter to continue.");
-        Console.ReadLine();
+            // // download 
+            // Console.WriteLine("==================1. DOWNLOAD==================");
+            // string url = "https://files.zip-codes.com/_updates_a9dh38dyq6ek/2020-06-01_UPDATE_UZJFV7B6/2020-06-Update-STANDARD.zip";
+            // string fileName = DBUpdate.DownloadFile(url);
+            // if (!String.IsNullOrEmpty(fileName))
+            // {
+            //     Console.WriteLine("Successfully download '" + fileName+"'");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Failed to download '"+fileName+"'");
+            //     // Application.Exit();
+            // }    
+            // Console.WriteLine("press Enter to continue.");
+            // Console.ReadLine();
 
-        // unzip
-        Console.WriteLine("====================2.UNZIP===================");
-        ExtractFile();
-        Console.WriteLine("Successfully unzip the file");
-        Console.WriteLine("press Enter to continue.");
-        Console.ReadLine();
+            // // unzip
+            // Console.WriteLine("====================2.UNZIP===================");
+            // ExtractFile();
+            // Console.WriteLine("Successfully unzip the file");
+            // Console.WriteLine("press Enter to continue.");
+            // Console.ReadLine();
 
 
-        
-        //create connection
-        Console.WriteLine("====================3.UNDATE===================");
-        string connectString = "Data Source=127.0.0.1,1433;Initial Catalog=SybottDB;User ID=SA;Password=1Secure*Password1";
-        SqlConnection conn = new SqlConnection(connectString);
-        conn.Open();
+            
+            //create connection
+            Console.WriteLine("====================3.UNDATE===================");
+            string connectString = "Data Source=127.0.0.1,1433;Initial Catalog=SybottDB;User ID=SA;Password=1Secure*Password1";
+            SqlConnection conn = new SqlConnection(connectString);
+            conn.Open();
 
-        //create data adapter
-        SqlDataAdapter myDataAdapter = new SqlDataAdapter("select * from SybottDB.dbo.ZIPCodes", conn);
-        SqlCommandBuilder mySqlCommandBuilder = new SqlCommandBuilder(myDataAdapter); 
+            //create data adapter
+            SqlDataAdapter myDataAdapter = new SqlDataAdapter("select * from SybottDB.dbo.ZIPCodes", conn);
+            SqlCommandBuilder mySqlCommandBuilder = new SqlCommandBuilder(myDataAdapter); 
 
-        //create and fill dataset        
-        DataSet myDataSet = new DataSet();
-        myDataAdapter.Fill(myDataSet,"ZIPCodes");
+            //create and fill dataset        
+            DataSet myDataSet = new DataSet();
+            myDataAdapter.Fill(myDataSet,"ZIPCodes");
 
-        //get data table reference
-        DataTable myTable = myDataSet.Tables["ZIPCodes"];
+            //get data table reference
+            DataTable myTable = myDataSet.Tables["ZIPCodes"];
+            logger.Info("The Total Number of Items BEFORE Updating: "+ myTable.Rows.Count);
 
-        //read from [update].tab file
-        var reader = new StreamReader(_unzipFileDirectory+"test.tab");
-        var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        csv.Configuration.Delimiter = "\t";
-        csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToLower();
-        var records = csv.GetRecords<UpdateTable>();
-        foreach (var row in records)
-        {
-            if (row.Type == 'D')
+            //read from [update].tab file
+            var reader = new StreamReader(_unzipFileDirectory+"test.tab");
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csv.Configuration.Delimiter = "\t";
+            csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToLower();
+            var records = csv.GetRecords<UpdateTable>();
+            foreach (var row in records)
             {
-                DeleteDataRow(myTable, row);
+                if (row.Type == 'D')
+                {
+                    DeleteDataRow(myTable, row);
 
+                }
+                else
+                {
+                    AddDataRow(myTable, row);
+                }
+                
             }
-            else
-            {
-                AddDataRow(myTable, row);
-            }
+
+            //sumbit local changes to SQL Server
+            myDataAdapter.Update(myDataSet,"ZIPCodes");
+            myTable.AcceptChanges();
+            logger.Info("The Total Number of Items AFTER Updating: "+ myTable.Rows.Count);
+
+            myDataSet.Dispose();
+            myDataAdapter.Dispose();
+            conn.Close();
+            conn.Dispose();
             
         }
+        catch (FileNotFoundException ex)
+        {
+            logger.Error(ex);
+        }
+        catch ( System.Data.SqlClient.SqlException ex)
+        {
+            logger.Error(ex);
+        }
+        // catch (Exception ex)
+        // {
+        //     // NLog: catch any exception and log it.
+        //     logger.Error(ex, "Stopped program because of an exception");
+        // }
+        finally
+        {
+            // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+            LogManager.Shutdown();
+        }
 
-        //sumbit local changes to SQL Server
-        myDataAdapter.Update(myDataSet,"ZIPCodes");
-        myTable.AcceptChanges();
-        Console.WriteLine(myTable.Rows.Count);
-
-        myDataSet.Dispose();
-        myDataAdapter.Dispose();
-        conn.Close();
-        conn.Dispose();
     }
-
 }
