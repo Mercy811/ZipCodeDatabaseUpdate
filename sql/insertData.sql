b0402944e440d81b19ef636136a8012e6e0683e4
Xinyi Ye <xinyiye811@gmail.com>
2020-08-31 15:23:15 +0800

    [milestone] local version 2. Make sure the zipcode is 5, no cut-off the leading 0

--- sql/insertData.sql
+++ sql/insertData.sql
@@ -1,9 +1,9 @@
 USE SybottDB;
 GO
-BULK INSERT SybottDB.dbo.ZIPCodes FROM '/usr/data/zipcode.csv'
+BULK INSERT SybottDB.dbo.ZIPCodes FROM '/usr/data/zip-codes-database-STANDARD-aug.csv'
    WITH (
       FIELDTERMINATOR = ',',
-      ROWTERMINATOR = '\n',
-      FORMAT = 'CSV'
+      ROWTERMINATOR = '0x0a',
+      FIRSTROW = 2
 );
 GO
--- sql/select.sql
+++ sql/select.sql
@@ -1,2 +1,2 @@
 SELECT * FROM [SybottDB].[dbo].[ZIPCodes]
-WHERE ZipCode='501' AND City='HOLTSVILLE' AND County='SUFFOLK' AND CityAliasName='HOLTSVILLE' AND PreferredLastLineKey='V13916' AND CityStateKey='V13916';
+WHERE ZipCode='92007' AND City='CARDIFF BY THE SEA' AND County='SAN DIEGO' AND CityAliasName='CARDIFF' AND PreferredLastLineKey='019093' AND CityStateKey='Z20504';
